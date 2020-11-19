f905ac9a7b1e6c47235b7746db997c9ed9f0f155
gaspilara <gaspii.lara@gmail.com>
2020-11-19 20:03:23 -0300

    I do not remember what I do here

---
+++ templates_c/70fa70244c911b3e7d0d1dc1dea56d99906878de_0.file.head.tpl.php
@@ -0,0 +1,48 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-11-19 23:28:25
+  from 'C:\xampp\htdocs\WEB 2\TPE\templates\public\head.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5fb6f189204c52_10196269',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '70fa70244c911b3e7d0d1dc1dea56d99906878de' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\WEB 2\\TPE\\templates\\public\\head.tpl',
+      1 => 1602257419,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5fb6f189204c52_10196269 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="<?php echo BASE_URL;?>
+">
+    <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+    <link rel="icon" href="img/WAV-logo.svg" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <?php echo '<script'; ?>
+ src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="https://kit.fontawesome.com/3404ee6456.js" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+
+    <?php echo '<script'; ?>
+ src="js/main2.js"><?php echo '</script'; ?>
+>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body><?php }
+}
